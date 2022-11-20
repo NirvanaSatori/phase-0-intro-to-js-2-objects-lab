fcd9ec7b07e839c536743fa53de9908a2de6373b
MORRIS <morris24kariuki@gmail.com>
2022-11-20 17:48:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Write your solution in this file!
+//Write your solution in this file!
+const employee = {Jane : "90 carli",John : "39 Austin",};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {
+        ...employee,
+        [key]: value,
+      };
+}
+const newEmployees = updateEmployeeWithKeyAndValue(employee, "Jane","100 Austin");
+
+employees.Jane;
+//newEmployees.Jane;
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){ 
+    employee[key] = value; 
+    return employee;
+};
+newerEmployees=destructivelyUpdateEmployeeWithKeyAndValue(
+    employees,
+    "John",
+    "40 illinois",);
+
+employee.John;    
+newerEmployees.John;
+
+function deleteFromEmployeeByKey(employee, key) {
+    return delete employee.key;
+};
+
+const newestEmployees =deleteFromEmployeeByKey(
+    employee,
+    "John");
+
+newestEmployees;
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+
+    return delete employee.key;
+}
+const updatedEmployees =deleteFromEmployeeByKey(
+    employee,
+    "John");
+updatedEmployees;
